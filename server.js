e998705ac2d7245ba23e4dff327607ef877772bb
schmitz1193 <schmitz1193@gmail.com>
2016-02-15 13:08:50 -0600

    mongodb setup

--- server.js
+++ server.js
@@ -7,7 +7,22 @@ const mongoose = require('mongoose');
 const routes = require('./routes/');  //slash as the end indicated you look in index first
 
 const PORT = process.env.PORT || 3000;  //set up environment port if nonne given default to 3000
-const MONGODB_URL = 'mongodb://localhost:27017/node-webserver';
+
+
+//set up mongodb URL
+
+const MONGODB_HOST = process.env.MONGODB_HOST || `localhost`;
+const MONGODB_PORT = process.env.MONGODB_PORT || 27017;
+const MONGODB_USER = process.env.MONGODB_USER || '';
+const MONGODB_PASS = process.env.MONGODB_PASS || '';
+const MONGODB_NAME = process.env.MONGODB_NAME || `node-webserver`;
+
+const MONGODB_AUTH = MONGODB_USER
+  ? `${MONGODB_USER}:${MONGODB_PASS}@`
+  : '';
+
+const MONGODB_URL = `mongodb://${MONGODB_AUTH}${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_NAME}`;
+/////////////////////////
 
 app.set('view engine', 'jade');  //set up engine to compile JADE
 
